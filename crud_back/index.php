1161e0cb84a838c9a786390d184570a4be4cf163
Daniel Oliveira <danielsapup3@gmail.com>
2021-07-21 23:37:42 -0300

    Front enviando requisicao para o back, recebendo as informacao e imprimindo

--- crud_back/index.php
+++ crud_back/index.php
@@ -1,6 +1,32 @@
 <?php
 
+//Cabeçalhos obrigatorios
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
 
 //Incluir a conexão
 include_once 'conexao.php';
+
+$query_produtos = "SELECT id, titulo, descricao FROM produtos ORDER BY id DESC";
+$result_produtos = $conn->prepare($query_produtos);
+$result_produtos->execute();
+
+if(($result_produtos) AND ($result_produtos->rowCount() != 0)){
+    while($row_produto = $result_produtos->fetch(PDO::FETCH_ASSOC)){
+        extract($row_produto);
+
+        $lista_produtos["records"][$id] = [
+            'id' => $id,
+            'titulo' => $titulo,
+            'descricao' => $descricao
+        ];
+    }
+    //Resposta com status 200
+    http_response_code(200);
+
+    //Retorna os produtos em formato json
+    echo json_encode($lista_produtos);
+}
+
+
 ?>
--- crud_front/src/App.js
+++ crud_front/src/App.js
@@ -1,9 +1,48 @@
+import React, { useState, useEffect } from "react";
 
 
 function App() {
+  
+  const [data, setData] = useState([]);
+  
+  const getProdutos = async () => {
+    fetch("http://localhost/crud_back/index.php")
+    .then((response) => response.json())
+    .then((responseJson) => (
+      //console.log(responseJson),
+      setData(responseJson.records)
+    ));
+  }
+  
+  useEffect(() => {
+    getProdutos();
+  },[])
+  
+  
+  
   return (
     <div>
       <h1>Listar</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Título</th>
+            <th>Descrição</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.values(data).map(produto => (
+            <tr key={produto.id}>  
+              <td>{produto.id}</td>
+              <td>{produto.titulo}</td>
+              <td>{produto.descricao}</td>
+              <td>Visualizar Editor Apagar</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
